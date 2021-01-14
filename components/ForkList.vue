3aa41c865548293f650c8e6a87f0c425f4b82443
mktcode <kontakt@markus-kottlaender.de>
2021-01-14 21:37:01 +0100

    added create fork link and stars count

--- components/ForkList.vue
+++ components/ForkList.vue
@@ -2,8 +2,13 @@
   <transition name="fade" mode="in-out">
     <div class="overlay" v-if="showForkList" @click="$store.commit('setShowForkList', false)">
       <div class="card shadow-sm fork-list" @click.stop>
-        <div class="card-body p-2">
-          <h5 class="text-center text-muted-light py-3 px-4">Explore the ecosystem. Here's a list of all OctoBay forks.</h5>
+        <div class="card-body px-2 pt-2 pb-0">
+          <h5 class="text-center text-muted-light py-3 px-4 m-0">Explore the ecosystem. Here's a list of all OctoBay forks.</h5>
+          <div class="text-right">
+            <small>
+              <a href="https://github.com/mktcode/octobay/fork" target="_blank" class="font-weight-bold">create fork</a>
+            </small>
+          </div>
         </div>
         <div class="card-header border-0 p-2">
           <input type="text" class="form-control form-control-lg" placeholder="Search" v-model="forkSearch" />
@@ -19,7 +24,13 @@
           <a v-for="fork in filteredForkList" :class="'mt-2 text-left d-flex align-items-center btn btn-outline-light text-dark'" :href="`https://${fork.username}.github.io/${fork.repository}`">
             <div target="_blank" class="rounded-circle shadow-sm avatar border" :style="'background-image: url(' + fork.logo + ')'"></div>
             <div class="ml-2 mr-auto d-flex flex-column">
-              {{ fork.username }}
+              <div class="d-flex justify-content-between">
+                {{ fork.username }}
+                <small class="text-muted">
+                  <small><font-awesome-icon :icon="['fas', 'star']" /></small>
+                  {{ fork.stars }}
+                </small>
+              </div>
               <small>{{ fork.description }}</small>
             </div>
           </a>
@@ -75,9 +86,11 @@ export default {
     ...mapGetters(['showForkList', 'forks']),
     filteredForkList() {
       const search = this.forkSearch.toLowerCase()
-      return this.forkSearch ?
+      const forkList = this.forkSearch ?
         this.forks.filter(fork => fork.username.toLowerCase().includes(search)).splice(0, this.showNum) :
         this.forks.filter(fork => fork).splice(0, this.showNum)
+      forkList.sort((a, b) => a.stars < b.stars)
+      return forkList
     }
   }
 }
