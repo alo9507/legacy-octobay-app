9fd91cf46c09cfc2321e0ae927c019f695b52c60
mktcode <kontakt@markus-kottlaender.de>
2021-04-10 21:49:40 +0200

    implemented voting

--- components/Proposal.vue
+++ components/Proposal.vue
@@ -18,7 +18,7 @@
               <b>{{ discussionNode.title }}</b>
             </div>
             <div
-              class="flex-fill rounded-lg position-relative"
+              class="flex-fill rounded-lg position-relative my-1"
               style="background-color: #ddd"
             >
               <div
@@ -35,19 +35,32 @@
               ></div>
             </div>
             <div class="d-flex justify-content-between">
-              <small class="text-muted"
-                >Quorum: {{ (proposal.quorum / 100).toFixed(2) }} %</small
-              >
-              <small class="text-muted"
-                >Current:
-                {{
-                  (
-                    proposal.votes.reduce((a, b) => a + b.percentage, 0) / 100
-                  ).toFixed(2)
-                }}
-                %</small
-              >
-              <small class="text-muted">Days left: 2</small>
+              <small class="text-muted text-center">
+                Quorum:<br />
+                <b>{{ (proposal.quorum / 100).toFixed(2) }} %</b>
+              </small>
+              <small class="text-muted text-center">
+                Current:<br />
+                <b>
+                  {{
+                    (
+                      proposal.votes.reduce((a, b) => a + b.percentage, 0) / 100
+                    ).toFixed(2)
+                  }}
+                  %
+                </b>
+              </small>
+              <small class="text-muted text-center">
+                Days left:<br />
+                <b>
+                  {{
+                    (
+                      (proposal.endDate - proposal.startDate) /
+                      (60 * 60 * 24)
+                    ).toFixed(1)
+                  }}
+                </b>
+              </small>
             </div>
           </div>
         </div>
@@ -67,12 +80,19 @@
             class="border-top border-bottom w-100 py-2 text-nowrap d-flex justify-content-between align-items-center px-4"
           >
             <div>
-              <!-- votes -->
+              <!-- vote -->
               <button
                 v-tooltip="{ content: 'Vote for proposal', trigger: 'hover' }"
                 class="btn btn-sm btn-success"
+                :disabled="waitingForVoteTransaction"
+                @click="castVote(false)"
               >
-                <font-awesome-icon :icon="['fas', 'thumbs-up']" />
+                <font-awesome-icon
+                  v-if="waitingForVoteTransaction"
+                  :icon="['fas', 'circle-notch']"
+                  spin
+                />
+                <font-awesome-icon v-else :icon="['fas', 'thumbs-up']" />
               </button>
               <button
                 v-tooltip="{
@@ -80,8 +100,15 @@
                   trigger: 'hover',
                 }"
                 class="btn btn-sm btn-danger"
+                :disabled="waitingForVoteTransaction"
+                @click="castVote(true)"
               >
-                <font-awesome-icon :icon="['fas', 'thumbs-down']" />
+                <font-awesome-icon
+                  v-if="waitingForVoteTransaction"
+                  :icon="['fas', 'circle-notch']"
+                  spin
+                />
+                <font-awesome-icon v-else :icon="['fas', 'thumbs-down']" />
               </button>
             </div>
             <div>
@@ -127,10 +154,8 @@
                     class="d-flex justify-content-between"
                   >
                     <small>
-                      <b>{{ vote.githubUser }}</b
-                      ><br />
                       <AddressShort
-                        :address="vote.address"
+                        :address="vote.holder.ethAddress"
                         class="text-muted"
                       />
                     </small>
@@ -194,10 +219,16 @@ export default {
       showDetails: false,
       action: null,
       loading: false,
+      waitingForVoteTransaction: false,
     }
   },
   computed: {
-    ...mapGetters(['account', 'registeredAccount', 'oracles']),
+    ...mapGetters([
+      'account',
+      'registeredAccount',
+      'selectedDepartment',
+      'oracles',
+    ]),
     ...mapGetters('github', {
       githubUser: 'user',
       githubAccessToken: 'accessToken',
@@ -209,7 +240,6 @@ export default {
       .then((discussion) => {
         if (discussion) {
           this.discussionNode = {
-            id: discussion.id,
             title: discussion.title,
             url: discussion.url,
           }
@@ -224,6 +254,27 @@ export default {
       } else {
         this.action = action
       }
+    },
+    castVote(against) {
+      this.waitingForVoteTransaction = true
+      this.$octobayGovToken(this.selectedDepartment.tokenAddress)
+        .methods.balanceOfAsPercentAt(
+          this.account,
+          this.proposal.balanceSnapshotId
+        )
+        .call()
+        .then((percentage) => {
+          this.$octobayGovernor.methods
+            .castVote(
+              this.selectedDepartment.projectId,
+              this.proposal.count,
+              against ? percentage * -1 : percentage
+            )
+            .send({ from: this.account })
+            .then(() => {
+              this.waitingForVoteTransaction = false
+            })
+        })
     },
   },
 }
