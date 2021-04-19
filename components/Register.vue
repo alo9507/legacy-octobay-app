8e63a9ca5e5a4e7c9b1fe7aa32f33a81c1dfdce8
mktcode <kontakt@markus-kottlaender.de>
2021-04-20 00:51:20 +0200

    cleaned up register component

--- components/Register.vue
+++ components/Register.vue
@@ -14,128 +14,58 @@
         You can now delete the repository again and start claiming funds.
       </small>
     </div>
-    <div v-if="connected">
-      <div v-if="githubUser">
-        <h5 class="text-center text-muted-light py-2 px-4">
-          Create a repository with your address as its name.
-        </h5>
-        <div
-          v-clipboard="account"
-          v-clipboard:success="copiedAddress"
-          class="d-flex justify-content-between align-items-center btn btn-light mt-3"
-        >
-          <transition name="fade" mode="out-in">
-            <font-awesome-icon
-              v-if="copyAddressSuccess"
-              key="check"
-              :icon="['fas', 'check']"
-              class="text-success"
-            />
-            <font-awesome-icon v-else key="copy" :icon="['far', 'copy']" />
-          </transition>
-          <i class="my-auto">
-            <AddressShort :address="account" length="medium"
-          /></i>
-          <i></i>
-        </div>
-        <a
-          href="https://github.com/new"
-          target="_blank"
-          class="d-flex justify-content-between align-items-center btn btn-dark btn-block mt-2"
-        >
-          <font-awesome-icon :icon="['fab', 'github']" />
-          Create Repository
-          <i></i>
-        </a>
-        <div class="text-center my-2">
-          <button
-            class="btn btn-outline-primary btn-sm font-weight-bold px-3"
-            @click="showIssueInput = !showIssueInput"
-          >
-            Want to withdraw a deposit?
-            <small
-              ><font-awesome-icon
-                :icon="['fas', showIssueInput ? 'times' : 'chevron-down']"
-                style="opacity: 0.5"
-            /></small>
-          </button>
-        </div>
-        <transition name="fade">
-          <div v-if="showIssueInput">
-            <div class="input-with-embed">
-              <input
-                v-model="issueUrl"
-                type="text"
-                class="form-control form-control-lg form-control-with-embed mb-2 pr-5"
-                placeholder="Issue URL"
-              />
-              <a
-                v-if="issue"
-                href="#"
-                class="position-absolute text-muted-light"
-                style="top: 12px; right: 10px; z-index: 2"
-                @click="
-                  issue = null
-                  issueUrl = ''
-                "
-              >
-                <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
-                  <path
-                    fill="currentColor"
-                    d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-                  />
-                </svg>
-              </a>
-              <div v-if="loadingIssue || issue">
-                <div v-if="loadingIssue" class="text-center mb-2">
-                  <font-awesome-icon
-                    :icon="['fas', 'circle-notch']"
-                    spin
-                    class="text-muted-light"
-                  />
-                </div>
-                <IssueEmbed v-if="issue" :issue="issue" />
-              </div>
-            </div>
-          </div>
-        </transition>
-        <button
-          class="btn btn-lg btn-primary shadow-sm d-block w-100 mt-3"
-          :disabled="loadingRegistration || checkingRepo"
-          @click="register()"
-        >
+    <h5 class="text-center text-muted-light py-2 px-4">
+      Create a repository with your address as its name.
+    </h5>
+    <div v-if="githubUser && connected">
+      <div
+        v-clipboard="account"
+        v-clipboard:success="copiedAddress"
+        class="d-flex justify-content-between align-items-center btn btn-light mt-3"
+      >
+        <transition name="fade" mode="out-in">
           <font-awesome-icon
-            v-if="loadingRegistration || checkingRepo"
-            :icon="['fas', 'circle-notch']"
-            spin
+            v-if="copyAddressSuccess"
+            key="check"
+            :icon="['fas', 'check']"
+            class="text-success"
           />
-          {{
-            loadingRegistration
-              ? 'Waiting for confirmation...'
-              : checkingRepo
-              ? 'Waiting for repository'
-              : issue && showIssueInput
-              ? 'Register & Withdraw'
-              : 'Register'
-          }}
-        </button>
+          <font-awesome-icon v-else key="copy" :icon="['far', 'copy']" />
+        </transition>
+        <i class="my-auto">
+          <AddressShort :address="account" length="medium"
+        /></i>
+        <i></i>
       </div>
       <a
-        v-else
-        :href="githubAuthUrl"
-        class="btn btn-lg btn-dark shadow-sm d-block mt-4"
+        href="https://github.com/new"
+        target="_blank"
+        class="d-flex justify-content-between align-items-center btn btn-dark btn-block mt-2"
       >
         <font-awesome-icon :icon="['fab', 'github']" />
-        Connect
+        Create Repository
+        <i></i>
       </a>
     </div>
-    <button
-      v-else-if="$web3"
-      class="btn btn-lg btn-primary shadow-sm d-block w-100 mt-4"
-      @click="connect()"
+    <ConnectActionButton
+      :action="register"
+      :disabled="loadingRegistration || checkingRepo"
+      :required="['wallet', 'github']"
+      class="mt-4"
     >
-      Connect Wallet
-    </button>
+      <font-awesome-icon
+        v-if="loadingRegistration || checkingRepo"
+        :icon="['fas', 'circle-notch']"
+        spin
+      />
+      {{
+        loadingRegistration
+          ? 'Waiting for confirmation...'
+          : checkingRepo
+          ? 'Waiting for repository'
+          : 'Register'
+      }}
+    </ConnectActionButton>
   </div>
 </template>
 
@@ -156,7 +86,6 @@ export default {
       repoExists: false,
       checkRepoInterval: null,
       registerRequestID: null,
-      showIssueInput: false,
       issueUrl: '',
       issue: null,
       loadingIssue: false,
