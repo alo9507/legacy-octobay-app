bbf974157e3ceaf2d914083790bc710ac89e1481
mktcode <kontakt@markus-kottlaender.de>
2021-04-26 17:07:16 +0200

    removed unused methods

--- mixins/helpers.js
+++ mixins/helpers.js
@@ -1,32 +1,5 @@
 export default {
   methods: {
-    getAge(createdAt) {
-      return (
-        (new Date().getTime() - new Date(createdAt).getTime()) /
-        (60 * 60 * 24 * 1000)
-      )
-    },
-    daysBetween(first, second) {
-      // Copy date parts of the timestamps, discarding the time parts.
-      const one = new Date(
-        first.getFullYear(),
-        first.getMonth(),
-        first.getDate()
-      )
-      const two = new Date(
-        second.getFullYear(),
-        second.getMonth(),
-        second.getDate()
-      )
-
-      // Do the math.
-      const millisecondsPerDay = 1000 * 60 * 60 * 24
-      const millisBetween = two.getTime() - one.getTime()
-      const days = millisBetween / millisecondsPerDay
-
-      // Round down.
-      return Math.floor(days)
-    },
     brightnessByColor(color) {
       color = '' + color
       const isHEX = color.indexOf('#') === 0
