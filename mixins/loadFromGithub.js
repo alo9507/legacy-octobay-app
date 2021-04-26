c526c359a098741cb0f931f6468c64894e9c88a5
mktcode <kontakt@markus-kottlaender.de>
2021-04-26 17:04:22 +0200

    removed unused method

--- mixins/loadFromGithub.js
+++ mixins/loadFromGithub.js
@@ -2,13 +2,6 @@ import axios from 'axios'
 
 export default {
   methods: {
-    loadPullRequest(owner, repo, number) {
-      return axios
-        .get(
-          `${process.env.API_URL}/github/pullrequest/${owner}/${repo}/${number}`
-        )
-        .then((res) => res.data)
-    },
     loadIssue(owner, repo, number) {
       return axios
         .get(`${process.env.API_URL}/github/issue/${owner}/${repo}/${number}`)
