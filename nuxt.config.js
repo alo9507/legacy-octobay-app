85da0128b6d58ee957e2909aaba35103a57f2d4c
mktcode <kontakt@markus-kottlaender.de>
2021-04-28 04:15:09 +0200

    remove unused var

--- nuxt.config.js
+++ nuxt.config.js
@@ -118,7 +118,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
-    extend(config, ctx) {
+    extend(config) {
       config.node = {
         fs: 'empty',
       }
