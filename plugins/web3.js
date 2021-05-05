c7e68d1bb6fb0017df2ebe5e35c6b0916d20de6c
mktcode <kontakt@markus-kottlaender.de>
2021-05-05 14:37:10 +0200

    Revert "initialized contracts only when connected to correct chain"
    This reverts commit bb2830a5d8c8e09ed814304157359784bc54c9f6.

--- plugins/web3.js
+++ plugins/web3.js
@@ -1,7 +1,22 @@
 const Web3 = require('web3')
 
-const injectContracts = async (inject, app, store) => {
-  if (store.state.networkId === process.env.CHAIN_ID) {
+export default async ({ app, store }, inject) => {
+  /**
+   * Inject standalone web3 utils (for non-web3 browsers)
+   */
+  inject('web3utils', Web3.utils)
+
+  if (window.ethereum) {
+    inject('web3', new Web3(window.ethereum))
+
+    window.ethereum.on('accountsChanged', () => {
+      store.dispatch('updateAccounts')
+    })
+
+    window.ethereum.on('chainChanged', () => {
+      store.dispatch('updateNetworkId')
+    })
+
     /**
      * Initialize Octobay Contract
      * In components: this.$octobay
@@ -43,36 +58,6 @@ const injectContracts = async (inject, app, store) => {
       (address) =>
         new app.$web3.eth.Contract(process.env.OCTOBAY_GOV_TOKEN_ABI, address)
     )
-  } else {
-    /**
-     * Set the contracts to null if connected to wrong network (for some in-template checks to work)
-     */
-    inject('octobay', null)
-    inject('octobayGovernor', null)
-    inject('octobayGovNFT', null)
-    inject('octobayGovToken', null)
-  }
-}
-
-export default ({ app, store }, inject) => {
-  /**
-   * Inject standalone web3 utils (for non-web3 browsers)
-   */
-  inject('web3utils', Web3.utils)
-
-  if (window.ethereum) {
-    inject('web3', new Web3(window.ethereum))
-    injectContracts(inject, app, store)
-
-    window.ethereum.on('accountsChanged', () => {
-      store.dispatch('updateAccounts')
-    })
-
-    window.ethereum.on('chainChanged', () => {
-      store
-        .dispatch('updateNetworkId')
-        .then(injectContracts(inject, app, store))
-    })
   } else {
     /**
      * Set web3 to null for non-web3 browsers (for some in-template checks to work)
