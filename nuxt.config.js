c2385c517889bd4d0f667d0c1fce865ec02d5af0
mkt <kontakt@markus-kottlaender.de>
2021-05-20 19:17:08 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'Octobay',
+    title: 'Octobay App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
