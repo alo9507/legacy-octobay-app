ba7de9d329b8596eddee5ad1fc865079b8ff101f
mktcode <kontakt@markus-kottlaender.de>
2021-05-26 05:59:37 +0200

    updated about text

--- components/About.vue
+++ components/About.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="about pb-md-5 mb-md-5">
     <div class="intro container">
-      <h4 class="text-center text-secondary">A Brave New Economy</h4>
-      <h1 class="text-center">Incentivized Open Source Collaboration</h1>
+      <h4 class="text-center text-secondary">
+        Workflow Driven Open Source Economy
+      </h4>
+      <h1 class="text-center">
+        Smart Bounties, Atomic Contracting &amp; more...
+      </h1>
       <p class="text-center lead pt-4 text-muted">
-        Octobay is a collaboration platform connecting contributors with open
-        source projects. Active contributors are rewarded with Bounties for
-        their accepted submissions.
+        Powered by <span class="text-secondary">Ethereum</span> and
+        <span class="text-secondary">GitHub</span>, Octobay provides new and
+        improved ways to benefit from open source collaboration and reduces
+        delays and counterparty risks in contracting by integrating payments
+        directly into your existing workflows and deployment pipelines.
       </p>
     </div>
   </div>
